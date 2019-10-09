db00ed146bf28d9377e4a29ac56faeb8bc9c8a7e
OrlandoMatteo <matteo.orlando.1993@gmai.com>
2019-10-09 15:22:08 +0200

    Added some docstrings

--- OOP/Exercise_4.py
+++ OOP/Exercise_4.py
@@ -1,4 +1,5 @@
 class Contact():
+	"""Contact defined by his name surname and emal"""
 	def __init__(self,name,surname,mail):
 		self.name=name
 		self.surname=surname
@@ -7,6 +8,14 @@ class Contact():
 		return "{},{},{}".format(self.name,self.surname,self.mail)
 
 class AddressBook():
+	"""Collection of object of the class Contact
+	The available methods are:
+	show():shows the list of contacts
+	find_by_name():returns all the contact with that name
+	find_by_surname():returns all the contact with that surname
+	add_contact(name,surname,email):adds the contact to the book
+	remove_contact(name): remove all the contacts with the given name
+	save():saves the book"""
 	def __init__(self):
 		fileContent=open('contacts.txt').read()
 		self.contacts=[]
@@ -15,32 +24,35 @@ class AddressBook():
 			contact_raw=line.split(',')
 			self.add_contact(contact_raw[0],contact_raw[1],contact_raw[2])
 	def show(self):
+		"""shows the list of contacts"""
 		for contact in self.contacts:
 			print(contact)
 	
 	def find_by_name(self,name):
+		"""find_by_name():returns all the contact with that name"""
 		results=[contact for contact in self.contacts if contact.name==name]
 		print("I found the following results:\n")
 		for x in results:
 			print(x)
 	def find_by_surname(self,surname):
+		"""find_by_surname():returns all the contact with that surname"""
 		results=[contact for contact in self.contacts if contact.surname==surname]
 		print("I found the following results:\n")
 		for x in results:
 			print(x)
 	def add_contact(self,name,surname,mail):
 		"""
-		new_contact(name,surname,mail):
+		add_contact(name,surname,email):adds the contact to the book
 		"""
 		self.contacts.append(Contact(name,surname,mail))
 		
 	def remove_contact(self,name):
-		"""remove_contact(name)"""
+		"""remove_contact(name): remove all the contacts with the given name"""
 		for i in range(len(self.contacts)):
 			if self.contacts[i].name==name:
 				self.contacts.pop(i)
 	def save(self):
-		"""save()"""
+		"""save():saves the book"""
 		fp=open('contacts.txt','w')
 		for c in self.contacts:
 			s=c.name+','+c.surname+','+c.mail+'\n'
--- OOP/Exercise_7.py
+++ OOP/Exercise_7.py
@@ -1,21 +1,25 @@
 import math
 class Circle(object):
-	"""docstring for Circle"""
+	"""Create a circle knowing his radius"""
 	def __init__(self, radius):
 		self.radius=radius
 	def perimeter(self):
+		"""Returns the perimeter of the circle"""
 		return (2*math.pi*self.radius)
 	def area(self):
+		"""Returns the area of the circle"""
 		return (math.pi*self.radius**2)
 
 class Cylinder(Circle):
-	"""docstring for Cylinder"""
+	""""Create a cylinder knowing his radius and his height"""
 	def __init__(self, radius,height):
 		Circle.__init__(self,radius)
 		self.height=height
 	def area(self):
+		"""Returns the area of the cylinder"""
 		return (self.perimeter()*self.height+2*Circle(self.radius).area())
 	def volume(self):
+		"""Returns the volume of the cylinder"""
 		return (Circle(self.radius).area()*self.height)
 
 if __name__ == '__main__':
