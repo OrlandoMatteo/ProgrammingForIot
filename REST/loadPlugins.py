8adb13ef2f3e875f818fe1f2a72715aab0583fd5
OrlandoMatteo <matteo.orlando.1993@gmail.com>
2019-10-25 11:02:26 +0200

    minor

--- REST/loadPlugins.py
+++ REST/loadPlugins.py
@@ -17,14 +17,14 @@ if __name__ == '__main__':
 				'tools.staticdir.root': os.path.abspath(os.getcwd()),
 			},
 		###If you remove the content below you will have a nicer look and the functions for the html page
-		# '/css':{
-		# 'tools.staticdir.on': True,
-		# 'tools.staticdir.dir':'./css'
-		# },
-		# '/js':{
-		# 'tools.staticdir.on': True,
-		# 'tools.staticdir.dir':'./js'
-		# },
+		 '/css':{
+		 'tools.staticdir.on': True,
+		 'tools.staticdir.dir':'./css'
+		 },
+		 '/js':{
+		 'tools.staticdir.on': True,
+		 'tools.staticdir.dir':'./js'
+		 },
 	}		
 	cherrypy.tree.mount(Example(),'/',conf)
 	cherrypy.engine.start()
