07e22c3313d483afee453672a005f2020f52318f
Matteo Orlando <matteo.orlando.1993@gmail.com>
2020-09-27 12:07:27 +0200

    Removed live Exercises

--- Training/LiveExercise/OOPLive/firstMain.py
+++
@@ -1,3 +0,0 @@
-if __name__ == '__main__':
-	a=input("Write a number ")
-	print(f"you wrote {a}")
--- Training/LiveExercise/OOPLive/main.py
+++
@@ -1,9 +0,0 @@
-from pointClass import *
-if __name__ == '__main__':
-	a=Point(2,2)
-	b=Point(2,8)
-	dist=a.distance(b)
-	print(dist)
-	a.move(2,2)
-	print(a.x,a.y)
-	
