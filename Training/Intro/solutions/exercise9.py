b27a77db098774be1eb7e7120b1d1035bfb50e69
Matteo Orlando <matteo.orlando.1993@gmail.com>
2020-10-01 09:21:07 +0200

    Minor

--- Training/Intro/solutions/exercise9.py
+++ Training/Intro/solutions/exercise9.py
@@ -8,8 +8,8 @@ def div(a,b):
 	print(f"{a} / {b} = {a/b:.3}")
 
 if __name__=="__main__":
-	a=2
-	b=7
+	a=input("Insert the first number")
+	b=input("Insert the second number")
 	add(a,b)
 	sub(a,b)
 	mul(a,b)
